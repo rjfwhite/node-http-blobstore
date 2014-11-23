5b0393723364a0939a663dc87d0c7f179f871edb
Rob Whitehead <rob@improbable.io>
2014-11-23 15:37:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+var busboy = require('connect-busboy');
+//...
+app.use(busboy()); 
+//...
+app.post('/fileupload', function(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.redirect('back');
+        });
+    });
+});
